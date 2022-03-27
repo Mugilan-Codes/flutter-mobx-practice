befb9b5d024df8988eead167aa4b5210dbd6693c
Mugilan E.S <mugilancodes@gmail.com>
2022-03-27 19:42:08 +0530

    add dice counter assets

--- lib/screens/dice_counter/dice_counter_screen.dart
+++ lib/screens/dice_counter/dice_counter_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:mobx_practice/screens/dice_counter/dice_counter.dart';
 import 'package:mobx_practice/widgets/navigation_drawer_widget.dart';
+
+final diceCounter = DiceCounter();
 
 class DiceCounterScreen extends StatelessWidget {
   const DiceCounterScreen({Key? key}) : super(key: key);
@@ -9,12 +13,47 @@ class DiceCounterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.amber,
       appBar: AppBar(
         title: Text(title),
+        backgroundColor: Colors.amberAccent,
       ),
       drawer: const NavigationDrawerWidget(),
       body: Center(
-        child: Text('Dice Counter'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                    child: TextButton(
+                        onPressed: diceCounter.roll,
+                        child: Observer(
+                          builder: (_) => Image.asset(
+                              'assets/images/dice/dice${diceCounter.left}.png'),
+                        ))),
+                Expanded(
+                    child: TextButton(
+                        onPressed: diceCounter.roll,
+                        child: Observer(
+                          builder: (_) => Image.asset(
+                              'assets/images/dice/dice${diceCounter.right}.png'),
+                        )))
+              ],
+            ),
+            Padding(
+                padding: const EdgeInsets.all(16),
+                child: Observer(
+                    builder: (_) => Text(
+                          'Total ${diceCounter.total}',
+                          style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black87,
+                              fontFamily: 'Verdana'),
+                        )))
+          ],
+        ),
       ),
     );
   }
